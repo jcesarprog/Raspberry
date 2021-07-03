42bc6e51cbbf5a0f6a736266cd143ffdd9f5038c
Julio Teixeira <jcesarprog@gmail.com>
2021-07-03 08:06:34 -0300

    updated wget in 05_ftp.sh

--- 05_ftp.sh
+++ 05_ftp.sh
@@ -10,7 +10,7 @@ sudo cp /etc/vsftpd.conf /etc/vsftpd.conf.original
 sudo rm /etc/vsftpd.conf
 
 # ? Copy here the basic vsftpd.conf file from github
-sudo wget -O /etc/vsftpd.conf https://raw.githubusercontent.com/jcesarprog/Raspberry/dev/scripts/vsftpd.conf
+sudo wget -O /etc/vsftpd.conf https://raw.githubusercontent.com/jcesarprog/Raspberry/master/vsftpd.conf
 
 # ? Creating the rsa file for secure connections valid for 1 year
 sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/vsftpd.pem -out /etc/ssl/private/vsftpd.pem
