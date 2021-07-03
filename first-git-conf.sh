ddd4ef3630f13f745ee5234ef1aa7e66a13feaee
Julio Teixeira <jcesarprog@gmail.com>
2021-07-03 07:58:07 -0300

    moved 2 scripts to untracket test folder

--- first-git-conf.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-git config --global user.name "Julio Teixeira"
-git config --global user.email "jcesarprog@gmail.com"
-git config --global core.editor vim
-echo "Listing git configurations"
-git config --list --show-origin
-
-# git config credential.helper store
